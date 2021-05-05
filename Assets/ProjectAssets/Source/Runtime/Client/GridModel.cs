56ec352d9e5d39e028bd7ea3e77de06855e532c6
Evan Osborn <evan@osborn.net>
2021-05-05 15:40:19 -0700

    MVP Architecture

---
+++ Assets/ProjectAssets/Source/Runtime/Client/GridModel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TicTacToe.Client.Runtime
+{    
+    public sealed class GridModel
+    {
+        
+        public readonly CellModel[,] CellArray = new CellModel[3, 3];
+        
+        
+    }
+
+}
