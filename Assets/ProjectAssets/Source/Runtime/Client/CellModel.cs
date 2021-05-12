660a0caf0e7300afe8afcd2bb33bddb59f5fdeba
Evan Osborn <evan@osborn.net>
2021-05-12 11:49:18 -0700

    Architecture and enum

--- Assets/ProjectAssets/Source/Runtime/Client/CellModel.cs
+++ Assets/ProjectAssets/Source/Runtime/Client/CellModel.cs
@@ -4,32 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace TicTacToe.Client.Runtime
-{
+{      
     
-    public enum Side
-    {
-        X,
-        O
-    }
-      
     public sealed class CellModel
     {
+
         public readonly Side PlayerSide = default;
 
         public CellModel(Side side)
         {
             PlayerSide = side;
         }   
-
-    }
-
-    public sealed class CellPresenter : MonoBehaviour
-    {
-        [SerializeField] private Text m_text = default;
-
-        public void Show(CellModel model)
-        {
-            m_text.text = model.PlayerSide.ToString();
-        }
     }
 }
