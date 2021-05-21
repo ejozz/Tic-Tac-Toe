f73e395bf50c4b9493bd2bfc6ef766ecafb79b72
Evan Osborn <evan@osborn.net>
2021-05-21 12:50:22 -0700

    Issue #26, fixed hierarchy and seperated click handling.

--- Assets/ProjectAssets/Source/Runtime/Client/CellPresenter.cs
+++ Assets/ProjectAssets/Source/Runtime/Client/CellPresenter.cs
@@ -4,62 +4,15 @@ using UnityEngine.UI;
 
 namespace TicTacToe.Client.Runtime
 {
-    public sealed class CellPresenter : MonoBehaviour, IPointerClickHandler
+    public sealed class CellPresenter : MonoBehaviour
     {
         [SerializeField] private Text m_text = default;
-        [SerializeField] private int m_xPosition;
-        [SerializeField] private int m_yPosition;
-
-        public delegate void EventHandler(CellPresenter sender);
-
-        public static event EventHandler OnClick;
-
-        //Detects click and invokes event
-        public void OnPointerClick(PointerEventData pointerEventData)
-        {
-            Debug.Log("Clicked on (" + m_xPosition + "," + m_yPosition + ")");
-
-            if (OnClick != null)
-            {
-                OnClick.Invoke(this);
-            }
-        }
-
+        
         //function sets text object to update view
-        public void Show(GridModel grid)
+        public void Show(CellModel model)
         {
-            m_text.text = grid.CellModelArray[m_xPosition, m_yPosition].PlayerSide.ToString();
+            m_text.text = model.PlayerSide.ToString();
         }
-
-        //function to set cell model side
-        public void SetCell(ref GridModel grid)
-        {
-            int m_randomNumber = Random.Range(0, 2);
-            //if X, sets to O
-            if (grid.CellModelArray[m_xPosition, m_yPosition].PlayerSide == Side.X)
-            {
-                grid.CellModelArray[m_xPosition, m_yPosition] = new CellModel(Side.O);
-            }
-
-            //if O, sets to X
-            else if (grid.CellModelArray[m_xPosition, m_yPosition].PlayerSide == Side.O)
-            {
-                grid.CellModelArray[m_xPosition, m_yPosition] = new CellModel(Side.X);
-            }
-
-            //else sets X or O randomly
-            else
-            {
-                if (m_randomNumber == 1)
-                {
-                    grid.CellModelArray[m_xPosition, m_yPosition] = new CellModel(Side.O);
-                }
-
-                else
-                {
-                    grid.CellModelArray[m_xPosition, m_yPosition] = new CellModel(Side.X);
-                }
-            }
-        }
+        
     }
 }
---
+++ Assets/ProjectAssets/Source/Runtime/Client/GridPosition.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TicTacToe.Client.Runtime
+{
+    public sealed class GridPosition : MonoBehaviour
+    {
+        
+        [SerializeField] private Vector2Int m_position = default;
+        public Vector2Int Value => m_position;
+        
+    }
+}
