9e6fc0544aa686e7eb745f2f2d3ad53377378491
Evan Osborn <evan@osborn.net>
2021-05-26 14:20:16 -0700

    Added animations w/ DOTween.

--- Assets/ProjectAssets/Source/Runtime/Client/CellPresenter.cs
+++ Assets/ProjectAssets/Source/Runtime/Client/CellPresenter.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace TicTacToe.Client.Runtime
 {
@@ -7,9 +8,14 @@ namespace TicTacToe.Client.Runtime
     {
         [SerializeField] private Text m_text = default;
         
+
         //function sets text object to update view
         public void Show(CellModel model)
         {
+            GetComponent<SpriteRenderer>().DOFade(0.55f,0.25f);
+            GetComponentInChildren<Text>().DOFade(0f, 0f);
+            GetComponentInChildren<Text>().DOFade(1.0f,0.25f);
+            
             m_text.text = model.PlayerSide.ToString();
         }
         
