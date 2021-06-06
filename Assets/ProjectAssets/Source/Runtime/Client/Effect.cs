7415c7a50084dc458ad57194a730bc236078d26f
Evan <evan@osborn.net>
2021-06-06 11:33:34 -0700

    Fixed comments

--- Assets/ProjectAssets/Source/Runtime/Client/Effect.cs
+++ Assets/ProjectAssets/Source/Runtime/Client/Effect.cs
@@ -5,11 +5,8 @@ namespace TicTacToe.Client.Runtime
 {
     public abstract class Effect : MonoBehaviour
     {        
-        private Sequence m_effectSequence;
-        
         public abstract void Play();
         
         public abstract void Stop();
-
     }
 }
