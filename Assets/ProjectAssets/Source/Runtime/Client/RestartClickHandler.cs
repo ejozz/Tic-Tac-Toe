15c2acdbb8f4f358491c6b34f04bbc0aa0ed04f2
Evan <evan@osborn.net>
2021-06-08 20:35:39 -0700

    Fixed commit

--- Assets/ProjectAssets/Source/Runtime/Client/RestartClickHandler.cs
+++ Assets/ProjectAssets/Source/Runtime/Client/RestartClickHandler.cs
@@ -7,7 +7,6 @@ namespace TicTacToe.Client.Runtime
     public sealed class RestartClickHandler : MonoBehaviour, IPointerClickHandler
     {
         public event Action OnClicked = default;
-
         public void OnPointerClick(PointerEventData eventData)
         {
             OnClicked();
